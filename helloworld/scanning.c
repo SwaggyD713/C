557d614c49d119a42a1c8c142a4bb622db0eaa77
Darioo <sofreski54321@gmail.com>
2024-11-28 11:15:06 +0100

    new scanning program

---
+++ helloworld/scanning.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+
+    printf("Please enter a number. \n");
+
+    scanf("%d", &num);
+
+    printf("The entered number is: %d\n", num);
+
+    getchar();
+
+    char c;
+
+    printf("Please enter a character. \n");
+
+    scanf("%c", &c);
+
+    printf("The entered character is: %c\n", c);
+
+
+    return 0;
+
+
+
+}
