61649b704e1e09fbe761d86b3910a04dd91d9b61
Darioo <sofreski54321@gmail.com>
2024-11-28 11:07:03 +0100

    changes

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -2,7 +2,22 @@
 
 int main() {
     /*My first program in C*/
-    printf("Hello, World! \n");
+
+    char hey = '!';
+
+    printf("Hello, World!%c \n", hey);
+
+    int x = 2147483647;
+    printf("x is: %d\n", x);
+
+    int xx = x+1;
+    printf("xx is: %d\n", xx);
+
+    unsigned int y = 4294967295;
+    printf("yy is: %u\n", y);
+
+    unsigned int yy = y+3;
+    printf("yy is: %u\n", yy);
 
     return 0;
 
